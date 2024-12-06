20f88d24d90431fa87b846bfd2a316545559d616
Ahmed-Fakhet <ahmed.fakhet@efrei.net>
2024-12-06 17:37:40 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	epreuves_mathematiques.py

--- epreuves_mathematiques.py
+++ epreuves_mathematiques.py
@@ -1,9 +1,21 @@
 import random
+a = random.randint(1,10)
+b = random.randint(1,10)
+def resoudre_equation_lineaire(a,b):
+    x = -b/a
+    return a,b,x
 
-def factorielle(n) :
-    if n == 0:
-            return 1
-    return n * factorielle(n - 1)
+S=False
+def epreuve_math_equation():
+    global a,b,S
+    a, b, x = resoudre_equation_lineaire(a,b)
+    print("Resoudre : ",a,"x +", b, "=", 0)
+    X=float(input("Saisir la solution X :"))
+    if x == X:
+        S=True
+    return S
+S=epreuve_math_equation()
+print(S)
 
 
 
