c79bbef3a4169be7466fb51d9914b0ffb5517b7d
Ahmed-Fakhet <ahmed.fakhet@efrei.net>
2024-12-12 19:28:57 +0100

    nouvelle fonction bonneteau

--- epreuves_hasard.py
+++ epreuves_hasard.py
@@ -1,4 +1,5 @@
 from random import
+
 def bonneteau():
     print("Bienvenue au jeu du Bonneteau !\nTrouvez la clé en 2 essais.")
     presence_clef = randint(1, 3)
@@ -16,4 +17,21 @@ def bonneteau():
         else:
             print(f"Raté ! Il vous reste {1 - essai} essai(s).")
     return False
+
+def jeu_lance_des():
+    essai = 3
+    des={1,2,3,4,5,6}
+    for i in range (3) :
+        print("vous avez",essai - i,"essais")
+        enter=input("appuyez sur entrer pour lancez les dés !")
+        des_joueur = {random.choice(des), random.choice(des)}
+        des_maitre = {random.choice(des), random.choice(des)}
+        print(f"Résultats du joueur : {des_joueur}")
+        if 6 in des_joueur:
+            print("Félicitations ! Vous avez obtenu un 6 et gagné la partie et donc la clé.")
+            return True
+            break
+
+
+
 
