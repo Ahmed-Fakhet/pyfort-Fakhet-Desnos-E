90bd192cb4ca64f9d9b2a2318cba2af833cc0288
Ahmed-Fakhet <ahmed.fakhet@efrei.net>
2024-12-31 19:48:43 +0100

    Ajout des commentaires

--- enigme_pere_fouras.py
+++ enigme_pere_fouras.py
@@ -1,8 +1,17 @@
-def charger_enigmes(fichier) :
-    with open("enigmesPF.json",'r') as fichier:
-        contenu = fichier.read()
-    L_enigmes = []
+def charger_enigmes(fichier):
+    L_enigmes= []
+    with open("enigmesPF.json","r") as fichier:
+        for ligne in fichier:
+            if "question" in ligne:
+                dico_enigme={}
+                question = ligne[21:len(ligne)-3]
+                ligne = fichier.readline()
+                reponse = ligne[20:len(ligne)-2]
+                dico_enigme[question] = reponse
+                L_enigmes.append(dico_enigme)
+    return L_enigmes
 
-def charger_enigmes(fichier) :
-    with open("enigmesPF.json",'r') as fichier:
-        for i in range (48) :
+
+
+def enigme_pere_fouras():
+    global L_enigmes
