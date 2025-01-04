00dcaea5319c91a50112cebb0faf7339c42a219d
desnoes corentin <corentin.desnoes@efrei.net>
2025-01-04 19:52:24 +0100

    commit

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ from fonctions_utiles import *
 def jeu():
     introduction()
     equipe = composer_equipe()
+    if not equipe :
+        print("Fin du jeu, il n'y a pas d'equipes ")
+    print("Votre équipe est prete a gagner, que le jeu commence!!")
+
+    cle_remportees = 0
+    historique = []
+    while cle_remportees < 3:
+        print("menu des épreuves")
+        choix_epreuve = menu_epreuves()
+
+
+
+
+
 
 
 
